45a79824e9325447ebd98dba47fae72fe8bf61b0
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2019-10-17 13:01:34 -0400

    Finished project section minus pictures

---
+++ components/Project.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="project">
+        <div class="img">
+            <img src="https://gradientjoy.com/150x150" alt="Image">
+        </div>
+        <div class="content">
+            <h3>{{title}}</h3>
+            <slot></slot>
+            <div class="technologies">Built with: {{techs}}</div>
+            <a class="githubRef" :href="url">View Source</a>
+        </div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Project",
+    props: {
+      title: String,
+      description: String,
+      url: String,
+      imageURL: String,
+      techs: String
+    }
+  }
+</script>
+
+<style scoped>
+    .project {
+        width: 80%;
+        background: white;
+        opacity: 0.8;
+        padding: 2em;
+        margin: 1em 0;
+        border-radius: 5px;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        transition: transform 0.5s;
+        -webkit-box-shadow: 7px 10px 5px 0px rgba(0,0,0,0.24);
+        -moz-box-shadow: 7px 10px 5px 0px rgba(0,0,0,0.24);
+        box-shadow: 7px 10px 5px 0px rgba(0,0,0,0.24);
+    }
+
+    .project:hover {
+        transform: scale(1.05);
+    }
+
+    h3 {
+        color: #75b9be;
+        margin: 0.5em 0;
+    }
+
+    .project:nth-child(odd) {
+        flex-direction: row-reverse;
+    }
+
+    a.githubRef {
+        color: #dd8b87;
+        text-decoration: none;
+    }
+    .technologies {
+        font-style: italic;
+        font-size: 0.8rem;
+        margin: 0.5em 0;
+    }
+
+</style>
--- components/Projects.vue
+++ components/Projects.vue
@@ -2,14 +2,54 @@
   <section id="projects">
     <div class="content">
       <h1>Projects</h1>
-      Wow look at these projects
+      <Project title="Attendance System"
+                url="https://github.com/kglovern/attendanceMobile"
+                techs="Typescript, Express, Kotlin, Anko"
+      >
+        <ul>
+          <li>Mobile app that can track attendance using the University of Guelph student card.</li>
+          <li>RESTful Web API written in Typescript accessible through mobile and web interfaces.</li>
+          <li>Mobile app written in Kotlin tracks attendance through unique barcodes belonging to each student.</li>
+        </ul>
+      </Project>
+      <Project title="Immediate Feedback System"
+               url="https://github.com/ian-james/IFS"
+               techs="Javascript, SQL, HTML, Python"
+      >
+        <ul>
+          <li>Allows the running of multiple verification tools against uploaded student code or written english assignments in order to receive feedback about potential errors.</li>
+          <li>Express server with AngularJS frontend.  Most tools written in Python.</li>
+          <li>Part of an ongoing research project into how first and second year students approach problem solving.</li>
+        </ul>
+      </Project>
+      <Project title="Prokudin-Gorskii Registration"
+               url="https://github.com/kglovern/pg_registration"
+               techs="Python, Numpy, SciKit"
+      >
+        <ul>
+          <li>Algorithm for the automatic linear registration of the Prokudin-Gorskii image set.</li>
+          <li>Results in colour photos with a very high SSIM rating to the original panes.</li>
+        </ul>
+      </Project>
+      <Project title="GEDCOM Parser"
+               url="https://github.com/kglovern/GEDCOM-system"
+               techs="C, Javascript, JQuery, SQL, HTML"
+      >
+        <ul>
+          <li>Web interface allowing the parsing of the GEDCOM genealogical format.</li>
+          <li>Parser written in C.</li>
+          <li>Client provided through Express is written in JQuery and interacts with the parser using a FFI.</li>
+        </ul>
+      </Project>
     </div>
   </section>
 </template>
 
 <script>
+import Project from "./Project"
 export default {
-    name: "Projects"
+  name: "Projects",
+  components: { Project },
 }
 </script>
 
@@ -21,5 +61,9 @@ export default {
     align-items: center;
     justify-content: center;
     background: #75b9be
+  }
+  li {
+    font-size: 0.8em;
+    margin: 1em 0;
   }
 </style>
